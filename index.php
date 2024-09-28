f146c86befb67fbfdb04755ad9beec58d65744fc
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-09-28 11:03:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$j = 0;
+
+do {
+    echo "Testando do while $j <br>";
+
+    $j++;
+} while($j < 10);
+
+$i = 10;
+
+do {
+    echo "Testando do while 2 <br>";
+    $i--;
+} while ($i > 0);
