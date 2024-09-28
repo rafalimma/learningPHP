e9d7e3035d4fb5f0d25d13430aade67ce6d1bb46
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-09-28 11:05:08 -0300

    Add files via upload

---
+++ foreach/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$nomes = ["Rafael", "João", "Maria", "Debora"];
+
+foreach($nomes as $nome) {
+    echo"O nome do indice atual é $nome <br>";
+    if ($nome == "Maria"){
+        echo "OPAPA";
+    }
+}
