51510ead8b21118e3382e6e24f2a2fefed4ba11f
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-10-05 22:16:39 -0300

    Add files via upload

---
+++ shottags/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// adicionar código php em uma página
+// depende de configurações de servidor
+echo "Testando código php nas tags normais<br>";
+?>
+
+<?
+echo "testando short tagss";
+?>
